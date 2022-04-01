8b9c897e9040d65739dd08622fe6c6caa0b26679
Nessvah <vanessamarquesns@outlook.pt>
2022-04-01 11:22:48 +0100

    add reset function

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Some variables
-let secretNumber = Math.trunc(Math.random() * 50 + 1);
+let randomNumber = Math.trunc(Math.random() * 50 + 1);
 let score = 20;
 let highscore = 0;
 const btnCheck = document.querySelector(".check");
@@ -22,9 +22,37 @@ btnCheck.addEventListener("click", function () {
     displayFeedback("🛑 No number.");
 
     // if player hits the number
-  } else if (guess === secretNumber) {
+  } else if (guess === randomNumber) {
     displayFeedback("🥳 That's the correct number!");
-    document.querySelector(".secret-number").textContent = secretNumber;
+    document.querySelector(".secret-number").textContent = randomNumber;
     document.body.style.backgroundColor = "#CBE896";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess !== randomNumber) {
+    if (score > 1) {
+      displayFeedback(guess > randomNumber ? "Too high!" : "Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayFeedback("You lost the game!");
+      document.querySelector(".score").textContent = 0;
+      document.body.style.backgroundColor = "#FD5D5D";
+    }
   }
 });
+
+// Reseting the game
+
+let btnAgain = document.querySelector(".restart");
+
+const restartGame = function (event) {
+  document.querySelector(".secret-number").textContent = "?";
+  document.querySelector(".score").textContent = "0";
+  document.body.style.backgroundColor = "#BEB7A4";
+  document.querySelector(".message").textContent = "Start guessing...";
+};
+
+btnAgain.addEventListener("click", restartGame);
