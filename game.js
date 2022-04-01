68c6787623b7216ac1864aa057ef16cb16158d97
Nessvah <vanessamarquesns@outlook.pt>
2022-04-01 11:54:37 +0100

    drying code

--- game.js
+++ game.js
@@ -7,25 +7,28 @@ let highscore = 0;
 const btnCheck = document.querySelector(".check");
 
 // Function to display feedback to user
-
-const displayFeedback = function (message) {
+const DISPLAYFEEDBACK = function (message) {
   document.querySelector(".message").textContent = message;
 };
 
-// Function to get input of the user on click
+// Function to change backgroundColor
+const CHANGEBACKCOLOR = function (color) {
+  document.body.style.backgroundColor = color;
+};
 
-btnCheck.addEventListener("click", function () {
+// Function to start playing the game
+const PLAYGAME = function (event) {
   const guess = Number(document.querySelector(".guess").value);
 
   // no number on input
   if (!guess) {
-    displayFeedback("🛑 No number.");
+    DISPLAYFEEDBACK("🛑 No number.");
 
     // if player hits the number
   } else if (guess === randomNumber) {
-    displayFeedback("🥳 That's the correct number!");
     document.querySelector(".secret-number").textContent = randomNumber;
-    document.body.style.backgroundColor = "#CBE896";
+    DISPLAYFEEDBACK("🥳 That's the correct number!");
+    CHANGEBACKCOLOR("#CBE896");
 
     if (score > highscore) {
       highscore = score;
@@ -33,26 +36,36 @@ btnCheck.addEventListener("click", function () {
     }
   } else if (guess !== randomNumber) {
     if (score > 1) {
-      displayFeedback(guess > randomNumber ? "Too high!" : "Too low!");
-      score--;
+      DISPLAYFEEDBACK(guess > randomNumber ? "Too high!" : "Too low!");
       document.querySelector(".score").textContent = score;
+      score--;
     } else {
-      displayFeedback("You lost the game!");
-      document.querySelector(".score").textContent = 0;
-      document.body.style.backgroundColor = "#FD5D5D";
+      DISPLAYFEEDBACK("You lost the game!");
+      CHANGEBACKCOLOR("#FD5D5D");
+      document.querySelector(".score").textContent = "0";
     }
   }
-});
+};
+
+btnCheck.addEventListener("click", PLAYGAME);
 
 // Reseting the game
+
+const changescore = function (number) {
+  document.querySelector(".score").textContent = number;
+};
 
 let btnAgain = document.querySelector(".restart");
 
 const restartGame = function (event) {
+  score = 20;
+  randomNumber = Math.trunc(Math.random() * 50 + 1);
+
   document.querySelector(".secret-number").textContent = "?";
-  document.querySelector(".score").textContent = "0";
-  document.body.style.backgroundColor = "#BEB7A4";
-  document.querySelector(".message").textContent = "Start guessing...";
+  changescore(0);
+  CHANGEBACKCOLOR("#BEB7A4");
+  DISPLAYFEEDBACK("Start guessing...");
+  document.querySelector(".guess").value = "";
 };
 
 btnAgain.addEventListener("click", restartGame);
