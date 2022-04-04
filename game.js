4c2826352db1ac76f51c6dc0a0a5d3a18a22f770
Nessvah <vanessamarquesns@outlook.pt>
2022-04-04 01:26:50 +0100

    fix score that wasnt updating correctly

--- game.js
+++ game.js
@@ -4,6 +4,7 @@
 let randomNumber = Math.trunc(Math.random() * 50 + 1);
 let score = 20;
 let highscore = 0;
+let btnAgain = document.querySelector(".restart");
 const btnCheck = document.querySelector(".check");
 
 // Function to display feedback to user
@@ -16,56 +17,63 @@ const CHANGEBACKCOLOR = function (color) {
   document.body.style.backgroundColor = color;
 };
 
+//Function to change score
+const CHANGESCORE = function (number) {
+  document.querySelector(".score").textContent = number;
+};
+
+const SHOWSECRETNUMBER = function (number) {
+  document.querySelector(".secret-number").textContent = number;
+};
+
 // Function to start playing the game
 const PLAYGAME = function (event) {
+  //get the user input
   const guess = Number(document.querySelector(".guess").value);
-
   // no number on input
   if (!guess) {
     DISPLAYFEEDBACK("🛑 No number.");
 
-    // if player hits the number
+    // if player guesses the number correctly
   } else if (guess === randomNumber) {
-    document.querySelector(".secret-number").textContent = randomNumber;
+    SHOWSECRETNUMBER(randomNumber);
     DISPLAYFEEDBACK("🥳 That's the correct number!");
     CHANGEBACKCOLOR("#CBE896");
 
-    if (score > highscore) {
+    //Updating the highscore
+    if (score >= highscore) {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
     }
+    // if the user doesnt't guesses the number correctly
   } else if (guess !== randomNumber) {
+    // If the score is higher than 1
     if (score > 1) {
-      DISPLAYFEEDBACK(guess > randomNumber ? "Too high!" : "Too low!");
-      document.querySelector(".score").textContent = score;
+      DISPLAYFEEDBACK(guess > randomNumber ? "Too high! 📈" : "Too low! 📉");
       score--;
+      CHANGESCORE(score);
     } else {
-      DISPLAYFEEDBACK("You lost the game!");
-      CHANGEBACKCOLOR("#FD5D5D");
-      document.querySelector(".score").textContent = "0";
+      //If the user runs out of guesses
+      DISPLAYFEEDBACK("You lost the game! 😢");
+      SHOWSECRETNUMBER(randomNumber);
+      CHANGEBACKCOLOR("#EB5D4E");
+      CHANGESCORE(0);
     }
   }
 };
 
 btnCheck.addEventListener("click", PLAYGAME);
 
-// Reseting the game
+// Function to reset the game and start over
 
-const changescore = function (number) {
-  document.querySelector(".score").textContent = number;
-};
-
-let btnAgain = document.querySelector(".restart");
-
-const restartGame = function (event) {
-  score = 20;
+btnAgain.addEventListener("click", function () {
   randomNumber = Math.trunc(Math.random() * 50 + 1);
+  score = 20;
+  console.log(score);
 
-  document.querySelector(".secret-number").textContent = "?";
-  changescore(0);
   CHANGEBACKCOLOR("#BEB7A4");
   DISPLAYFEEDBACK("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".secret-number").textContent = "?";
   document.querySelector(".guess").value = "";
-};
-
-btnAgain.addEventListener("click", restartGame);
+});
